69e4e4027a0a67f82f3607ea4ba6bebde5236473
toby lorne <toby@toby.codes>
2021-07-22 00:18:25 +0200

    office: add basic lists functionality
    Signed-off-by: toby lorne <toby@toby.codes>

---
+++ lists.rb
@@ -0,0 +1,29 @@
+require "sinatra"
+
+class Office < Sinatra::Base
+  get "/lists" do
+    @lists = List.all
+    erb :"lists/index"
+  end
+
+  post "/lists" do
+    l = List.create(title: params[:title], creator: current_user)
+    redirect "/lists/#{l.id}"
+  end
+
+  get "/lists/:id" do
+    @list = List.find(id: params[:id])
+    erb :"lists/show"
+  end
+
+  post "/lists/:id/items" do
+    l = List.find(id: params[:id])
+    l.add_item(title: params[:title], creator: current_user)
+    redirect "/lists/#{l.id}"
+  end
+
+  post "/lists/:list_id/items/:list_item_id/check" do
+    li = ListItem.find(id: params[:list_item_id], list_id: params[:list_id])
+    redirect "/lists/#{li.list_id}"
+  end
+end
