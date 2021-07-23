9c14e7a5612e9184551b1d6f7469634e6019928d
toby lorne <toby@toby.codes>
2021-07-23 08:43:40 +0200

    office: remove created at non nullable constraint
    this is set by the sequel plugin
    
    Signed-off-by: toby lorne <toby@toby.codes>

--- migrations/5_add_timestamps_to_lists_and_list_items_and_events.rb
+++ migrations/5_add_timestamps_to_lists_and_list_items_and_events.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   up do
     %i[events lists list_items].each do |tbl|
       alter_table tbl do
-        add_column :created_at, Time, null: false
+        add_column :created_at, Time
         add_column :updated_at, Time
       end
     end
